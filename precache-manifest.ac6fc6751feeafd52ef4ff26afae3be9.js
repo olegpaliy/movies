741822ca301cf60c3b171b516e922d392c5ceaa9
olegpaliy <goleh@ukr.net>
2022-08-24 12:50:42 +0300

    Updates

---
+++ precache-manifest.ac6fc6751feeafd52ef4ff26afae3be9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff06db062f2aa709767ffa11206cebfe",
+    "url": "/github.io/movies/index.html"
+  },
+  {
+    "revision": "59e5189b2551ec8317c0",
+    "url": "/github.io/movies/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "9be2568482fc1b1ae314",
+    "url": "/github.io/movies/static/js/2.6426c65b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/github.io/movies/static/js/2.6426c65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e5189b2551ec8317c0",
+    "url": "/github.io/movies/static/js/main.05beb609.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/github.io/movies/static/js/main.05beb609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cae777cfd8b737758da",
+    "url": "/github.io/movies/static/js/runtime-main.1eb63d93.js"
+  }
+]);
